85168c58e3ced48e306287719318f798df7c779f
Laurence_A <abre.svnoj@gmail.com>
2022-07-08 09:21:50 +0800

    Milestone 1: Add button

---
+++ app/src/main/java/com/example/backgroundabregana/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.backgroundabregana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /*public void myButtonListenerMethod() {
+        Button button = (Button) findViewById(R.id.btn_Change);
+        button.setOnClickListener(new View.OnClickListener()) {
+            //@Override
+
+        }
+    }*/
+
+}
